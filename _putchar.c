bfbb064078f6424ed5d5a1a3cb25949b1a496d01
Naikumi <naikumimartha@gmail.com>
2023-03-28 02:58:18 -0700

    printf project

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include <unistd>
+
+/**
+ * _putchar - prints char
+ * @c: the character to print
+ *
+ * Return: 1 on success, -1  error and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
